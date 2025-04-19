0b01b75d3100083239562af68ba182cb67971701
superbrucem <superbrucem@gmail.com>
2025-04-19 00:11:22 -0400

    Fix Netlify build issues with PostCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./views/**/*.ejs",
+    "./controllers/**/*.js"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#ffcccc', // Light red color
+        'secondary': '#333333',
+        'accent': '#f8f8f8',
+      },
+    },
   },
   plugins: [],
 }
